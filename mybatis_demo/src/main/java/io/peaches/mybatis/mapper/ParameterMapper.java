e7b6288aeb0fb710e26511914ce87210adf3f516
berior <1269921156@qq.com>
2022-04-09 18:22:52 +0800

    Update

--- mybatis_demo/src/main/java/io/peaches/mybatis/mapper/ParameterMapper.java
+++ mybatis_demo/src/main/java/io/peaches/mybatis/mapper/ParameterMapper.java
@@ -1,4 +1,13 @@
 package io.peaches.mybatis.mapper;
 
+import io.peaches.mybatis.pojo.User;
+
 public interface ParameterMapper {
+
+
+    /**
+     * 根据用户名查询
+     */
+    User getUserByUsername(String username);
+
 }
--- mybatis_demo/src/test/java/io/peaches/mybatis/test/ParameterMapperTest.java
+++ mybatis_demo/src/test/java/io/peaches/mybatis/test/ParameterMapperTest.java
@@ -1,6 +1,19 @@
 package io.peaches.mybatis.test;
 
+import io.peaches.mybatis.mapper.ParameterMapper;
+import io.peaches.mybatis.pojo.User;
+import io.peaches.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
 public class ParameterMapperTest {
 
-    
+    @Test
+    public void testParameterMapper() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        ParameterMapper mapper = sqlSession.getMapper(ParameterMapper.class);
+
+        User result = mapper.getUserByUsername("admin");
+        System.out.println(result);
+    }
 }
